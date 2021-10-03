8de64867ddd6aedde14a5af2c585a8bd4529704c
Thijs Van der Schaeghe <thijs@catlab.be>
2021-10-03 10:14:18 +0200

    Fix producer role translation.

--- resources/lang/nl/roles.php
+++ resources/lang/nl/roles.php
@@ -4,6 +4,7 @@ return [
     'author' => 'auteur',
     'presenter' => 'presentator',
     'musician' => 'muzikant',
+    'producer' => 'eindredactie'
 
     'authors' => 'Auteurs',
     'presentation' => 'Presentatie',
