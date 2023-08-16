19b209deda728488e0208ddcebcfb9c51ca82695
Thijs Van der Schaeghe <thijs@catlab.be>
2023-08-16 11:05:45 +0200

    Change typo in email invitation.

--- resources/views/events/waitingListInvitation.blade.php
+++ resources/views/events/waitingListInvitation.blade.php
@@ -10,18 +10,28 @@
 
     <h3>Pre-registration / waiting list</h3>
 
-    <p>Mail this to <code><a href="mailto:{{$user->email}}">{{ $user->email }}</a></code>.</p>
+    <p class="alert alert-info">
+        Je kan deze template gebruiken om de persoon op de wachtlijst te contacteren.
+        Stuur daarom een email naar: <code><a href="mailto:{{$user->email}}">{{ $user->email }}</a></code>.
+    </p>
+
+    <p class="alert alert-danger">
+        Deze email is nog niet verstuurd! Je dient deze manueel te versturen.
+    </p>
+
+    <pre>{{ $user->email }}</pre>
+
     <pre>Wachtlijst {{ $event->name }}</pre>
 
 <pre>
 Beste {{ $user->username }},
 
-Er is een ticket vrijgekomen voor {{ $event->name }} op {{ $event->startDate->format('m/d/Y') }} en jij staat als volgende op de wachtlijst.
+Er is een ticket vrijgekomen voor {{ $event->name }} op {{ $event->startDate->format('d/m/Y') }} en jij staat als volgende op de wachtlijst.
 
 Ben je nog geïnteresseerd in het ticket? Bestel het dan snel via
 {{ $url }}
 
-Wees er snel bij, want als je het niet binnen de 24u besteld gaat het naar de volgende.
+Wees er snel bij, want als je het niet binnen de 24u bestelt gaat het naar de volgende.
 Toch geen interesse? Stuur ons dan een mailtje terug, zodat wij de volgende kunnen uitnodigen.
 
 Veel succes!
