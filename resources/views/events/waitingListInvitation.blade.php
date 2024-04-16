8c3029d65af5f0ff205c0fc3405863792d76c428
Thijs Van der Schaeghe <thijs@catlab.be>
2024-04-16 16:09:46 +0200

    Fix invitation email.

--- resources/views/events/waitingListInvitation.blade.php
+++ resources/views/events/waitingListInvitation.blade.php
@@ -26,12 +26,11 @@
 <pre>
 Beste {{ $user->username }},
 
-Er is een ticket vrijgekomen voor {{ $event->name }} op
-    {{ $event->eventDates
+Er is een ticket vrijgekomen voor {{ $event->name }} op {{ $event->eventDates
         ->filter(function($date) { return $date->isSoldOut(); })
         ->map(function($date) { return $date->startDate->format('d/m/Y'); })
         ->join(' & ')
-    }} en jij staat als volgende op de wachtlijst.
+}} en jij staat als volgende op de wachtlijst.
 
 Ben je nog geïnteresseerd in het ticket? Bestel het dan snel via
 {{ $url }}
